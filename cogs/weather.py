e39dec6922d81be778c780129a593a652778c484
pinhead <mrmaxc2023@gmail.com>
2023-04-24 20:45:12 -0500

    Begin image creation

--- cogs/weather.py
+++ cogs/weather.py
@@ -17,6 +17,72 @@ async def urlify(s):
     return s
 
 
+async def determine_color(report):
+    alerts = report['alerts']
+    active_alert = False if len(alerts['alert']) == 0 else True
+    print(active_alert)
+    # weather_code = report['current']['condition']['code']  # current condition's code
+    weather_code = report['forecast']['forecastday'][0]['day']['condition']['code']  # current day's overall forecast code
+    is_day = report['current']['is_day']
+
+    if active_alert:
+        return discord.Color.red()
+    elif weather_code == 1000 and is_day == 1:  # clear and day
+        return 0xcad3f5  # barely grey
+    elif weather_code == 1000 and is_day == 0:  # clear and night
+        return 0x313c76  # dark blue
+
+    match weather_code:
+        # clouds/sky
+        case 1003:  # partly cloudy
+            return 0x939ab7  # light grey
+        case 1006:  # cloudy
+            return 0x6e738d  # medium grey
+        case 1009:  # overcast
+            return 0x494d64  # dark grey
+        case 1030 | 1063:  # mist, patchy rain possible
+            return 0xe4fcdc  # very light green
+
+        # rain
+        case 1150 | 1153 | 1180 | 1183 | 1240:  # patchy light drizzle, light drizzle, patchy light rain, light rain,
+            # light rain shower
+            return 0xa6da95  # light green
+        case 1186 | 1189 | 1273 | 1087:  # moderate rain at times, moderate rain, patchy light rain with thunder,
+            # thundery outbreaks possible
+            return 0x90ff6c  # green
+        case 1192 | 1195:  # heavy rain at times, heavy rain
+            return 0x5ede34  # dark green
+        case 1246 | 1276:  # torrential rain shower, moderate or heavy rain with thunder
+            return 0x40a02b  # very dark green
+
+        # snow/ice pellets
+        case 1135 | 1210 | 1213 | 1255 | 1261 | 1279:  # patchy light snow with thunder, patchy light snow, light snow,
+            # light showers of ice pellets, light snow showers
+            return 0xcae2fe  # very light blue
+        case 1114 | 1216 | 1219 | 1237:  # blowing snow, patchy moderate snow, moderate snow, ice pellets
+            return 0xb7bdf8  # light blue
+        case 1222 | 1225 | 1258 | 1264 | 1282:  # patchy heavy snow, heavy snow, moderate or heavy snow showers,
+            # moderate or heavy showers of ice pellets, moderate or heavy snow with thunder
+            return 0x8aadf4  # blue
+        case 1117 | 1147:  # blizzard, freezing fog
+            return 0x5483e2  # dark blue
+
+        # sleet/freezing
+        case 1069 | 1072 | 1198 | 1204 | 1249:  # patchy sleet possible, patchy freezing drizzle possible,
+            # light freezing rain, light sleet, light sleet showers
+            return 0xffd8f5  # light pink
+        case 1168:  # freezing drizzle
+            return 0xf5bde6  # lightish pink
+        case 1171, 1201, 1207:  # heavy freezing drizzle, moderate or heavy freezing rain, moderate or heavy sleet
+            return 0xf977d7  # pink
+        case _:
+            return 0x000000
+
+
+async def will_it_weather(likeliness):
+    return 'Yes' if likeliness == 1 else 'No'
+
+
 class Weather(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -28,53 +94,7 @@ class Weather(commands.Cog):
                                            "q": location,
                                            "alerts": "yes"
                                            }) as response:
-                if response.headers["CDN-Cache"] == "MISS":
-                    print("hell")
                 return await response.json()
-
-    # https://www.weatherapi.com/docs/weather_conditions.json
-    # color coding based on conditions
-    # cloud cover is greys
-    # rain is greens
-    # snow is light blues
-    # freezing is dark blues
-    # sleet is pinks
-    #
-    # Red if alerts (the highest priority)
-    #
-    # clouds
-    # 1000 = sunny = sky blue
-    # 1003 = partly cloudy = light grey
-    # 1006 = cloudy = med gray
-    # 1009 = overcast = dark grey
-    # 1000 = clear = slightly blue black
-    #
-    # rain
-    # 1030, 1063 = mist, patchy rain possible = barely light green
-    # 1150, 1153, 1180, 1183, 1240 = patchy light drizzle, light drizzle, patchy light rain, light rain, light rain shower = light green
-    # 1186, 1189, 1273, 1087 = moderate rain at times, moderate rain, patchy light rain with thunder, thundery outbreaks possible = green
-    # 1192, 1195 = heavy rain at times, heavy rain = darkish green
-    # 1246, 1276 = torrential rain shower, moderate or heavy rain with thunder = dark green
-    #
-    # snow
-    # 1210, 1213, 1255, 1279, 1135 = patchy light snow, light snow, light snow showers, patchy light snow with thunder = bright white
-    # 1216, 1219 = patchy moderate snow, moderate snow = slightly blue white
-    # 1222, 1225, 1258, 1282 = patchy heavy snow, heavy snow, moderate or heavy snow showers, moderate or heavy snow with thunder = notably blue white
-    #
-    # sleet
-    # 1069 = patchy sleet possible = slightly pink
-
-
-    async def determine_color(self, ctx, report):
-        alerts = report['alerts']
-        print(len(alerts['alert']))
-        active_alert = False if len(alerts['alert']) == 0 else True
-        if active_alert:
-            return discord.Color.red()
-
-        # switch
-
-        return 000000
 
     weather = SlashCommandGroup("weather", "Gets weather at a location")
 
@@ -89,7 +109,9 @@ class Weather(commands.Cog):
         forecast = report_current['forecast']
         forecast_today = forecast['forecastday'][0]['day']  # this is what it takes to get today's chance of rain
         rain_chance = forecast_today['daily_chance_of_rain']
+        rain_likely = await will_it_weather(forecast_today['daily_will_it_rain'])
         snow_chance = forecast_today['daily_chance_of_snow']
+        snow_likely = await will_it_weather(forecast_today['daily_will_it_snow'])
 
         embed = discord.Embed(title=f"{location['name']}, "f"{location['region']}",
                               description=location['country'],
@@ -98,10 +120,10 @@ class Weather(commands.Cog):
                                   f"{await urlify(location['region'])}%2C"
                                   f"{await urlify(location['country'])}",
                               timestamp=datetime.now(timezone.utc),
-                              color=await self.determine_color(ctx, report_current)
+                              color=await determine_color(report_current)
                               )
         timestring = datetime.fromtimestamp(current['last_updated_epoch']).strftime("%I:%M %p")
-        embed.set_footer(text=f"Data as of {timestring}")
+        embed.set_footer(text=f"Data as of {timestring} | Source: WeatherAPI.com")
         embed.set_thumbnail(url=f"https:{condition['icon']}")
         embed.add_field(name="Temperature",
                         value=f"*Current:* {current['temp_f']}°F\n"
@@ -116,19 +138,19 @@ class Weather(commands.Cog):
         if snow_chance == 0 and current['temp_f'] > 35:
             embed.add_field(name='Precipitation',
                             value=f"*Chance of Rain:* {rain_chance}%\n"
-                                  f"*Likely to Rain:* {'Yes' if forecast_today['daily_will_it_rain'] == 1 else 'No'}",
+                                  f"*Likely to Rain:* {rain_likely}",
                             inline=True)
         elif snow_chance > 0 and rain_chance > 0:
             embed.add_field(name='Precipitation',
                             value=f"*Chance of Rain:* {rain_chance}%\n"
-                                  f"*Likely to Rain:* {'Yes' if forecast_today['daily_will_it_rain'] == 1 else 'No'}"
+                                  f"*Likely to Rain:* {rain_likely}"
                                   f"*Chance of Snow:* {snow_chance}%\n"
-                                  f"*Likely to Snow:* {'Yes' if forecast_today['daily_will_it_snow'] == 1 else 'No'}",
+                                  f"*Likely to Snow:* {snow_likely}",
                             inline=True)
         else:
             embed.add_field(name='Precipitation',
                             value=f"*Chance of Snow:* {snow_chance}%\n"
-                                  f"*Likely to Snow:* {'Yes' if forecast_today['daily_will_it_snow'] == 1 else 'No'}",
+                                  f"*Likely to Snow:* {snow_likely}",
                             inline=True)
         await ctx.respond(embed=embed)
 
---
+++ images/image_report.py
@@ -0,0 +1,4 @@
+from PIL import Image
+
+img = Image.new(mode="RGB", size=(600, 400), color=(36, 39, 58))
+img.show()
